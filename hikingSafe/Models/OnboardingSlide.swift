af59b1885eba4df802dcb9afce2926cc0c7081d0
KKaay0708 <110819885+KKaay0708@users.noreply.github.com>
2023-08-01 14:54:05 -0700

    onboardingView setup

---
+++ hikingSafe/Models/OnboardingSlide.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingSlide.swift
+//  hikingSafe
+//
+//  Created by Kaitlyn Kwan on 8/1/23.
+//
+
+import Foundation
--- hikingSafe/Views/Onboarding/OnboardingViewController.swift
+++ hikingSafe/Views/Onboarding/OnboardingViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
+    }
+    
 }
